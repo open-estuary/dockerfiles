07b42ade61e4f0754f6922a095ae2b9819f21759
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-12 18:54:51 +0800

    add openjdk docker files

---
+++ openjdk/build_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tag_num=""
+if [ ! -z "${1}" ] ;  then
+    tag_num=":${1}"
+fi
+
+yum install -y -q docker
+service docker start
+
+docker build -t openestuary/openjdk${tag_num} .
+
